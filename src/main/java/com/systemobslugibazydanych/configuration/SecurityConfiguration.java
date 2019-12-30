a9a7921ad6eb3c3644905c83b0d9dc05e4835849
Albert Kozera <albertkozera97@gmail.com>
2019-12-30 07:11:19 +0100

    fix

--- src/main/java/com/systemobslugibazydanych/configuration/SecurityConfiguration.java
+++ src/main/java/com/systemobslugibazydanych/configuration/SecurityConfiguration.java
@@ -67,7 +67,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/resources/**", "/static/**", "/common/**", "/js/**", "/images/**");
+		web.ignoring().antMatchers("/resources/**", "/static/**", "/common/**", "/images/**");
 	}
 
 }
