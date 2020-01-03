94fd78f7357d9c4de995b2b7d0267f494dcb1291
Albert Kozera <albertkozera97@gmail.com>
2020-01-03 07:51:59 +0100

    fix

--- src/main/resources/static/diagramER/DBDesign.js
+++ src/main/resources/static/diagramER/DBDesign.js
@@ -96,6 +96,7 @@ $(document).ready(function () {
             table.setStroke("#ffffff");
             table.setStrokeDashStyle(DashStyle.Solid);
             table.setStrokeThickness(1.5);
+            table.setCaptionHeight(7.5);
 
             // set the first column to resize with the table
             table.columns[0] = {width: 5, columnStyle: 0};
@@ -433,6 +434,7 @@ function createTable() {
     table.setStroke("#ffffff");
     table.setStrokeDashStyle(DashStyle.Solid);
     table.setStrokeThickness(1.5);
+    table.setCaptionHeight(7.5);
 
     // set the first column to resize with the table
     table.columns[0] = {width: 5, columnStyle: 0};
