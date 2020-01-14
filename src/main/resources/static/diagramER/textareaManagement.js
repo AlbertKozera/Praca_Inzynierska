85ef82a5fd06b6c8d48f152c1e9c53cd795e09b2
Albert Kozera <albertkozera97@gmail.com>
2020-01-14 16:38:26 +0100

    fix

---
+++ src/main/resources/static/diagramER/textareaManagement.js
@@ -0,0 +1,20 @@
+var flag = true;
+$('#sidebarCollapse').click(function() {
+    if(flag){
+        $('#generatedSql').css({
+            'width': '901px'
+        });
+        flag = false;
+    }
+    else if(!flag){
+        setTimeout(function () {
+            $('#generatedSql').css({
+                'width': '601px'
+            });
+        }, 300);
+        flag = true;
+    }
+});
+
+
+
